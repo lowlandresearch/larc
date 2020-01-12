059e0457d0e98cb6cb507d9b73660fcfc766b7f4
David O'Gwynn <dogwynn@gmail.com>
2020-01-11 18:44:16 -0600

    added shell_pipe, fixed doctests

--- larc/shell.py
+++ larc/shell.py
@@ -1,10 +1,11 @@
 import os
+import io
 import sys
 import subprocess
 import shlex
 import logging
 from threading import Timer
-from typing import Callable, Any
+from typing import Callable, Any, List
 from pathlib import Path        # noqa: for doctest
 import tempfile                 # noqa: for doctest
 
@@ -15,6 +16,16 @@ from .common import cprint
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
+
+def start_timeout(command: List[str], process: subprocess.Popen,
+                  timeout: int):
+    # https://www.blog.pythonlibrary.org/2016/05/17/python-101-how-to-timeout-a-subprocess/
+    def kill():
+        log.warning(f'Process ({command[0]}) timeout expired.')
+        return process.kill()
+    timer = Timer(timeout, kill)
+    timer.start()
+    return timer
 
 @curry
 def shell_iter(command, *, echo: bool = True,
@@ -60,9 +71,8 @@ def shell_iter(command, *, echo: bool = True,
     ...     root = Path(tempdir)
     ...     _ = Path(root, 'a.txt').write_text('')
     ...     _ = Path(root, 'b.txt').write_text('')
+    ...     # FYI, this echos to stderr, which doctests won't capture
     ...     *lines, status = shell_iter(f'ls {root}')
-    a.txt
-    b.txt
     >>> lines
     ['a.txt', 'b.txt']
     >>> status
@@ -73,8 +83,8 @@ def shell_iter(command, *, echo: bool = True,
     ...     _ = Path(root, 'c.txt').write_text('')
     ...     _ = Path(root, 'd.txt').write_text('')
     ...     *lines, _ = shell_iter(f'ls {root}', echo=False)
-    >>> lines == ['c.txt', 'd.txt']
-    True
+    >>> lines
+    ['c.txt', 'd.txt']
 
     >>> *lines, status = shell_iter(
     ...     f'sleep 5', echo=False, timeout=0.01
@@ -96,16 +106,13 @@ def shell_iter(command, *, echo: bool = True,
         map(os.path.expandvars),
         tuple,
     )
+
     process = subprocess.Popen(command_split, **popen_kw)
 
+    timer = None
     if timeout:
-        # https://www.blog.pythonlibrary.org/2016/05/17/python-101-how-to-timeout-a-subprocess/
-        def kill():
-            log.warning(f'Process ({command_split[0]}) timeout expired.')
-            return process.kill()
-        timer = Timer(timeout, kill)
-        timer.start()
-    
+        timer = start_timeout(command_split, process, timeout)
+
     def process_running():
         return process.poll() is None
 
@@ -120,8 +127,9 @@ def shell_iter(command, *, echo: bool = True,
             else:
                 line += char
 
-    if timeout:
+    if timer:
         timer.cancel()
+        
     rest = process.stdout.read().decode('utf-8', errors='ignore')
     for char in rest:
         echo_func(char) if echo else ''
@@ -139,13 +147,9 @@ def shell_iter(command, *, echo: bool = True,
 
 @curry
 def shell(command, **kw):
-    '''Execute a shell command, yield lines of output as they come
-    possibly echoing command output to a given echo_func, and finally
-    yields the status code of the process.
-
-    This will run the shell command, yielding each line of
-    output. When the process terminates, it will then yield the status
-    code.
+    '''Execute a shell command and return status code as an int and
+    command output as a string, possibly echoing command output to a
+    given echo_func.
 
     Args:
 
@@ -169,11 +173,10 @@ def shell(command, **kw):
     ...     root = Path(tempdir)
     ...     _ = Path(root, 'a.txt').write_text('')
     ...     _ = Path(root, 'b.txt').write_text('')
-    ...     *lines, status = shell_iter(f'ls {root}')
-    a.txt
-    b.txt
-    >>> lines
-    ['a.txt', 'b.txt']
+    ...     # FYI, this echos to stderr, which doctests won't capture
+    ...     status, output = shell(f'ls {root}')
+    >>> output == "a.txt\\nb.txt"
+    True
     >>> status
     0
 
@@ -181,17 +184,18 @@ def shell(command, **kw):
     ...     root = Path(tempdir)
     ...     _ = Path(root, 'c.txt').write_text('')
     ...     _ = Path(root, 'd.txt').write_text('')
-    ...     *lines, _ = shell_iter(f'ls {root}', echo=False)
-    >>> lines == ['c.txt', 'd.txt']
+    ...     _, output = shell(f'ls {root}', echo=False)
+    >>> output == 'c.txt\\nd.txt'
     True
 
-    >>> *lines, status = shell_iter(
+    >>> status, output = shell(
     ...     f'sleep 5', echo=False, timeout=0.01
     ... )
-    >>> lines
-    []
+    >>> output == ""
+    True
     >>> status
     -9
+
     '''
     *lines, status = shell_iter(command, **kw)
     return status, '\n'.join(lines)
@@ -200,3 +204,61 @@ def shell(command, **kw):
 def getoutput(command, **kw):
     status, content = shell(command, **kw)
     return content
+
+
+@curry
+def shell_pipe(command, stdin, *, timeout: int = None, **popen_kw):
+    '''Execute a shell command with stdin content and return command
+    output as a string.
+
+    Args:
+
+      command (str): Shell command to execute. Tilde (~) and shell
+        variable completion provided
+
+      stdin (str): String content to provide to process stdin
+
+      timeout (int): If set, the process will be killed after this
+        many seconds (kill -9).
+
+    Examples:
+
+    >>> with tempfile.TemporaryDirectory() as tempdir:
+    ...     root = Path(tempdir)
+    ...     _ = Path(root, 'a.txt').write_text('')
+    ...     _ = Path(root, 'b.txt').write_text('')
+    ...     _ = Path(root, 'ab.txt').write_text('')
+    ...     output = pipe(
+    ...         getoutput(f'ls {root}'),
+    ...         shell_pipe('grep a')
+    ...     )
+    >>> sorted(output.strip().split()) == ["a.txt", "ab.txt"]
+    True
+
+    '''
+    popen_kw = merge({
+        'stdout': subprocess.PIPE,
+        'stderr': subprocess.PIPE,
+        'stdin': subprocess.PIPE,
+    }, popen_kw)
+
+    command_split = pipe(
+        shlex.split(command),
+        map(os.path.expanduser),
+        map(os.path.expandvars),
+        tuple,
+    )
+
+    process = subprocess.Popen(command_split, **popen_kw)
+
+    timer = None
+    if timeout:
+        timer = start_timeout()
+    stdout, stderr = process.communicate(
+        stdin.encode('utf-8', errors='ignore')
+    )
+
+    if timer:
+        timer.cancel()
+
+    return stdout.decode('utf-8', errors='ignore')
