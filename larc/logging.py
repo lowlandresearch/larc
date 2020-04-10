e85fe25403236e134c0f4dadd33a92e2638659b8
David O'Gwynn <dogwynn@belhaven.edu>
2020-04-10 15:21:26 -0500

    added new_long

--- larc/logging.py
+++ larc/logging.py
@@ -1,5 +1,12 @@
+import logging
+
 import coloredlogs
 from toolz import merge
+
+def new_log(name):
+    log = logging.getLogger(name)
+    log.addHandler(logging.NullHandler())
+    return log
 
 def setup_logging(loglevel: str, **config_kw):
     fmt = (
